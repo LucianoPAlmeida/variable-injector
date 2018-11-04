1be3b317d07f1307961d7b43a3b38d11a04e5e59
Luciano Almeida <passos.luciano@outlook.com>
2018-11-04 01:59:34 -0200

    Adding minor fixes

--- Tests/VariableInjectorTests/ArgumentsHandlerTests.swift
+++ Tests/VariableInjectorTests/ArgumentsHandlerTests.swift
@@ -9,22 +9,18 @@ final class ArgumentsHandlerTests: XCTestCase {
         let args = ["argument", "--file", "/file.swift", "--include", "PATH", "--exclude", "PATHS"]
         let arguments = ArgumentsHandler(args: args)
 
-        let expected = [Argument(name: "file", values: ["/file.swift"]),
-                        Argument(name: "include", values: ["PATH"]),
-                        Argument(name: "exclude", values: ["PATHS"])]
-
-        XCTAssertEqual(arguments._parsedArgs, expected)
+        XCTAssertEqual(arguments.argumentValues(for: "file"), ["/file.swift"])
+        XCTAssertEqual(arguments.argumentValues(for: "include"), ["PATH"])
+        XCTAssertEqual(arguments.argumentValues(for: "exclude"), ["PATHS"])
     }
     
     func testParseParametersWithEmpty() throws {
         let args = ["argument", "--file", "--include", "PATH", "--exclude", "PATHS"]
         let arguments = ArgumentsHandler(args: args)
         
-        let expected = [Argument(name: "file"),
-                        Argument(name: "include", values: ["PATH"]),
-                        Argument(name: "exclude", values: ["PATHS"])]
-        
-        XCTAssertEqual(arguments._parsedArgs, expected)
+        XCTAssertNil(arguments.argumentValue(for: "file"))
+        XCTAssertEqual(arguments.argumentValues(for: "include"), ["PATH"])
+        XCTAssertEqual(arguments.argumentValues(for: "exclude"), ["PATHS"])
     }
     
     static var allTests = [
