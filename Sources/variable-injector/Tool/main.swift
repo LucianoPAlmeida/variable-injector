c9ab86bfe265214eb4a697e3caa997657ab8a924
Luciano Almeida <passos.luciano@outlook.com>
2018-11-04 17:37:08 -0200

    Handling multiple files.

--- Sources/variable-injector/Tool/main.swift
+++ Sources/variable-injector/Tool/main.swift
@@ -5,25 +5,28 @@ import variable_injector_core
 // Handling arguments
 let arguments = ArgumentsHandler(args: CommandLine.arguments)
 
-guard let file = arguments.argumentValue(for: "file") else {
-    fatalError("The path to the file where the variables will be injected should be passed. Use --file $path-to-file")
+let files = arguments.argumentValues(for: "file")
+guard !files.isEmpty else {
+    fatalError("The path to at least one file where the variables will be injected should be passed. Use --file $path-to-file")
 }
 
 let varLiteralsToInclude = arguments.argumentValues(for: "include")
 let varLiteralsToIgnore = arguments.argumentValues(for: "ignore")
 
-// Loading file
-let url = URL(fileURLWithPath: file)
-let sourceFile = try SyntaxTreeParser.parse(url)
-
-let envVarRewriter = EnvirionmentVariableLiteralRewriter(ignoredLiteralValues: varLiteralsToIgnore)
-let result = envVarRewriter.visit(sourceFile)
-
-var contents: String = ""
-result.write(to: &contents)
-
-try? contents.write(to: url, atomically: true, encoding: .utf8)
-
-print("========================================================================\n")
-print(contents)
+// Loading files
+for file in files {
+    let url = URL(fileURLWithPath: file)
+    let sourceFile = try SyntaxTreeParser.parse(url)
+    
+    let envVarRewriter = EnvirionmentVariableLiteralRewriter(ignoredLiteralValues: varLiteralsToIgnore)
+    let result = envVarRewriter.visit(sourceFile)
+    
+    var contents: String = ""
+    result.write(to: &contents)
+    
+    try? contents.write(to: url, atomically: true, encoding: .utf8)
+    
+    print("========================================================================\n")
+    print(contents)
+}
 
