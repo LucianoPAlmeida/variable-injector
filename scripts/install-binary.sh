599c62cc41f57f5186e737b489e083fbcd2e91f0
Luciano Almeida <passos.luciano@outlook.com>
2019-04-23 21:10:05 -0300

    Updating install script.

--- scripts/install-binary.sh
+++ scripts/install-binary.sh
@@ -2,7 +2,7 @@ CURRENT_DIR="$PWD"
 cd /tmp
 curl -OL https://github.com/LucianoPAlmeida/variable-injector/releases/download/0.2.1/x86_64-apple-macosx.zip
 unzip x86_64-apple-macosx.zip
-cp -f .x86_64-apple-macosx/release/variable-injector /usr/local/bin/variable-injector
+cp -f ./x86_64-apple-macosx/release/variable-injector /usr/local/bin/variable-injector
 
 rm ./x86_64-apple-macosx.zip
 rm -rf ./x86_64-apple-macosx
