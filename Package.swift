db0e54e9dc3751de86b74b2c97fdbbfa38ecd32c
Luciano Almeida <passos.luciano@outlook.com>
2020-03-01 21:09:15 -0300

    [ArgumentParser] Implementing new library argument parser

--- Package.swift
+++ Package.swift
@@ -13,14 +13,15 @@ let package = Package(
         
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50100.0"))
+        .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50100.0")),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", .exact("0.0.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "variable-injector-core",
-            dependencies: ["SwiftSyntax"],
+            dependencies: ["SwiftSyntax", "ArgumentParser"],
             path: "Sources/variable-injector/Core"),
         .target(
             name: "variable-injector",
---
+++ Sources/variable-injector/Core/CommandLine/Tool.swift
@@ -0,0 +1,78 @@
+//
+//  ArgumentsHandler.swift
+//  VariableInjector
+//
+//  Created by Luciano Almeida on 03/11/18.
+//
+import Foundation
+import SwiftSyntax
+import ArgumentParser
+
+public struct VariableInjectorTool: ParsableCommand {
+  
+  public static let configuration = CommandConfiguration(abstract:
+    "Variable injector is a very simple project with the goal of inject CI" +
+    "pipelines environment variables values into Swift code static values before compilation")
+  
+  @Flag(name: NameSpecification(arrayLiteral: .customLong("verbose"), .customShort("v")),
+        help: "Indicates if the tool will run in verbose mode or not")
+  public var isVerbose: Bool
+  
+  @Option(name: NameSpecification(arrayLiteral: .customLong("file"), .customShort("f")),
+          help: "The path(s) to the file(s) where the tool should run.")
+  public var files: [String]
+  
+  @Option(name: NameSpecification(arrayLiteral: .customLong("ignore"), .customShort("i")),
+          help: "The names of variables that will be ignored even if that matches $(ENV_VAR) to avoid the replacing")
+  public var varLiteralsToIgnore: [String]
+    
+  public init() {}
+  
+  public mutating func validate() throws {
+    if files.isEmpty {
+      throw ValidationError("The path to at least one file where the variables will be injected should be provided. Use --file $path-to-file")
+    }
+  }
+  
+  public func run() throws {
+    //Separator
+    let printSeparator = "=" * 70
+    // Logger
+    var logger: Logger?
+    
+    if isVerbose {
+        logger = Logger()
+    }
+
+    // Loading files
+    for file in files {
+        let url = URL(fileURLWithPath: file)
+        
+        guard FileManager.default.fileExists(atPath: file) else {
+            logger?.log(message: "File not found. Skipping: \(url)")
+            continue;
+        }
+        
+        logger?.log(message: "\(printSeparator)\n")
+        logger?.log(message: "FILE: \(url.lastPathComponent)")
+        logger?.log(message: "\(printSeparator)\n")
+
+        let sourceFile = try SyntaxParser.parse(url)
+        
+        let envVarRewriter = EnvironmentVariableLiteralRewriter(
+                                environment: ProcessInfo.processInfo.environment,
+                                ignoredLiteralValues: varLiteralsToIgnore,
+                                logger: logger
+                             )
+        let result = envVarRewriter.visit(sourceFile)
+        
+        var contents: String = ""
+        result.write(to: &contents)
+        
+        try? contents.write(to: url, atomically: true, encoding: .utf8)
+        
+        logger?.log(message: "\(printSeparator)\n")
+        logger?.log(message: contents)
+    }
+  }
+}
